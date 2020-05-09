82322998d7972de8fee6c387c6c3bd9cfcd5ad29
Lambda_School_Loaner_282 <lambda_school_loaner_282@Lambda-School-Loaner-282s-MacBook-Pro.local>
2020-05-09 01:07:02 -0500

    First push.  Two events added to FunBus

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+const boatImage = document.querySelector(".img-fluid");
+console.log(boatImage);
+
+boatImage.addEventListener("mouseenter", () => {
+ boatImage.style.transform = "scale(1.2)";
+ boatImage.style.transition = "all 0.3s";
+});
+
+boatImage.addEventListener('mouseleave', () => {
+    boatImage.style.transform = "scale(1)";
+  })
+
+
